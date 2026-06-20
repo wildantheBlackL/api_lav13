1773ee5dda0a6598e505d6f87f425d4f92b170a9
unknown <hafidgameboy@gmail.com>
2026-06-20 22:15:24 +0700

    finishing bug

--- app/Http/Controllers/Utama.php
+++ app/Http/Controllers/Utama.php
@@ -18,10 +18,15 @@ class Utama extends Controller
     $id = floor(microtime(true) * 1000);
     $arr_data = explode("#", $dtx);
     $mx = new Mdata();
+    $url = "https://apiexpress-production-c1f5.up.railway.app/notifikasi-backup";
     $proses = $mx->tambahBackup($id, $nama, "laravel");
     if($proses == "1"){
         $berhasil = 0;
         $gagal = 0;
+        Http::post($url, [
+    'status' => 'mulai',
+    'total' => count($arr_data)
+]);
         foreach ($arr_data as $k){
             $arr_data2 = explode("|", $k);
             $idx = $arr_data2[0];
@@ -31,45 +36,25 @@ class Utama extends Controller
             $jenisx = $arr_data2[4];
             $proses2 = $mx->tambahTransaksi($id."-".$idx, $id, $waktux, $nominalx, $jenisx, $deskripsix);
             $proses2 == "1" ? $berhasil++ : $gagal++;
+            Http::post($url, [
+            'status' => 'progress',
+            'selesai' => $berhasil + $gagal,
+            'total' => count($arr_data),
+            'deskripsi' => $deskripsix
+        ]);
+             Http::post($url, [
+            'status' => 'selesai',
+            'berhasil' => $berhasil,
+            'gagal' => $gagal
+        ]);
         }
         $pesanx = ["kode"=>"01", "status"=>"Proses Backup Berhasil dengan Rincian ", "berhasil"=>$berhasil, "gagal"=>$gagal];
-        Http::post($url, [
-    'status' => 'mulai',
-    'total' => count($arr_data)
-]);
-
-$berhasil = 0;
-$gagal = 0;
-
-foreach ($arr_data as $k) {
-
-    $arr = explode("|", $k);
-
-    $deskripsi = $arr[1];
-
-    // proses DB
-    $proses2 = $mx->tambahTransaksi(...);
-
-    $proses2 == "1" ? $berhasil++ : $gagal++;
-
-    Http::post($url, [
-        'status' => 'progress',
-        'selesai' => $berhasil + $gagal,
-        'total' => count($arr_data),
-        'deskripsi' => $deskripsi
-    ]);
-}
-
-Http::post($url, [
-    'status' => 'selesai',
-    'berhasil' => $berhasil,
-    'gagal' => $gagal
-]);
         $kodex = 200;
     }else{
         $pesanx = ["kode"=>"00", "status"=>"Proses Backup Gagal, Periksa Kembali Data Anda"];
         $kodex = 500;
     }
+    
     return response()->json($pesanx, $kodex);
 }
 }
